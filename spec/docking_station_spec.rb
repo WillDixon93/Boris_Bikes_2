24498bfaba8ef7ea4f64259be41561026a9964da
AJ <aj.monty@gmail.com>
2020-11-25 08:42:51 +0000

    Diagram in user stories fixed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,9 @@
 require 'docking_station_class'
 
 describe DockingStation do
-<<<<<<< HEAD
     it "expects DockingStation to respond to the method release_bike" do
         p "hello world"
         expect(DockingStation).to respond_to{release_bike}
         p "hello will"
-=======
-  it "expects DockingStation to respond to the method release_bike" do
-    p "hello world"
-    expect(DockingStation).to respond_to { release_bike } 
-    p "hello will"
->>>>>>> d6b43bc0c576f6b8902f43a7b1e5d0d6c9b04618
     end
 end
