1813726071a9011cb4f92b62f920a4833f77c5ce
AJ <aj.monty@gmail.com>
2020-11-25 08:44:03 +0000

    Docking_station_spec tidied up, p's removed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,6 @@ require 'docking_station_class'
 
 describe DockingStation do
     it "expects DockingStation to respond to the method release_bike" do
-        p "hello world"
         expect(DockingStation).to respond_to{release_bike}
-        p "hello will"
     end
 end
